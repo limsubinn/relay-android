4286a9b3b5f36e99bfdca49806909f9e83fb0969
lana <artist158@naver.com>
2023-01-18 05:48:44 +0900

    Design: 개인정보 이용처리 xml (임시)
    PrivacyCondition 액티비티에 적용되는 xml 작성, 입력 내용과 형식은 임의로 작성 (수정 필요)

--- app/src/main/java/com/example/relay/login/SignupActivity.kt
+++ app/src/main/java/com/example/relay/login/SignupActivity.kt
@@ -19,20 +19,27 @@ class SignupActivity : AppCompatActivity() {
 
         // 개인정보이용동의 약관 액티비티로 전송 ( or sharedPreferences 사용 )
         viewBinding.btnNext.setOnClickListener{
+            val name = viewBinding.etName.text.toString()
+            val email = viewBinding.etEmail.text.toString()
+            val phone = viewBinding.etPhone.text.toString()
+            val pw = viewBinding.etPw.text.toString()
             val emailPattern = android.util.Patterns.EMAIL_ADDRESS;
-            if (emailPattern.matcher(viewBinding.etEmail.text.toString()).matches()) {
+
+            if (name.isBlank() || email.isBlank() || phone.isBlank() || pw.isBlank() || viewBinding.etCheckPw.text.toString().isBlank())
+                Toast.makeText(this, "입력되지 않은 칸이 존재합니다.", Toast.LENGTH_SHORT).show()
+            else if (!emailPattern.matcher(email).matches())
                 Toast.makeText(this, "이메일 형식이 올바르지 않습니다.", Toast.LENGTH_SHORT).show()
-            } else if (!viewBinding.etPw.text.toString().equals(viewBinding.etCheckPw.text.toString())){
+            else if (pw != viewBinding.etCheckPw.text.toString()){
                 Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
             } else if (false){
                 // (미구현) 서버와 통신해서 계정이 있는지 확인하기
                 Toast.makeText(this, "계정이 존재하는 이메일입니다.", Toast.LENGTH_SHORT).show()
             } else {
                 val intent = Intent(this, PrivacyConditionActivity::class.java)
-                intent.putExtra("name", viewBinding.etName.text.toString())
-                intent.putExtra("email", viewBinding.etEmail.text.toString())
-                intent.putExtra("phone", viewBinding.etPhone.text.toString())
-                intent.putExtra("pw", viewBinding.etPw.text.toString())
+                intent.putExtra("name", name)
+                intent.putExtra("email", email)
+                intent.putExtra("phone", phone)
+                intent.putExtra("pw", pw)
                 startActivity(intent)
             }
         }
