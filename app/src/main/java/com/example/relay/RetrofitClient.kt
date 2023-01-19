8b30030622ec5c029352ba3ef4e68b9020a30f42
lana <artist158@naver.com>
2023-01-19 18:12:56 +0900

    Feat: Retrofit 객체 생성
    싱글톤 패턴의 Retrofit 객체 생성, OkHttp 로깅 인터셉터 등록, OkHttp 관련 gradle 수정

---
+++ app/src/main/java/com/example/relay/RetrofitClient.kt
@@ -0,0 +1,36 @@
+package com.example.relay
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+// 싱글톤 패턴
+// 하나의 Retrofit 객체만를 생성하여 재사용
+object RetrofitClient {
+    private var instance: Retrofit?= null
+    private val CONNECT_TIMEOUT_SEC = 20000L
+
+    fun getInstance() : Retrofit {
+        if(instance == null){
+
+            // 로깅인터셉터 세팅
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+            // OKHttpClient에 로깅인터셉터 등록
+            val client = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .connectTimeout(CONNECT_TIMEOUT_SEC, TimeUnit.SECONDS)
+                .build()
+
+            instance = Retrofit.Builder()
+                .baseUrl("baseUrl 작성")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client) // Retrofit 객체에 OkHttpClient 적용
+                .build()
+        }
+        return instance!!
+    }
+}
