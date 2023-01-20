8df976195db129790445e1c8e06dccb7cd12850a
limsubinn <mymyx02@naver.com>
2023-01-20 14:00:50 +0900

    Design: 달력 디자인
    item_selected_calendar, item_unselected_calendar 추가

--- app/src/main/java/com/example/relay/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/relay/mypage/MypageFragment.kt
@@ -1,18 +1,33 @@
 package com.example.relay.mypage
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.relay.databinding.FragmentMypageBinding
+import com.michalsvec.singlerowcalendar.calendar.CalendarViewManager
+import com.michalsvec.singlerowcalendar.calendar.SingleRowCalendarAdapter
+import java.util.*
 
 class MypageFragment: Fragment() {
+    private val binding: FragmentMypageBinding by lazy {
+        FragmentMypageBinding.inflate(layoutInflater)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return FragmentMypageBinding.inflate(layoutInflater).root
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
 }
