5329296297bc9e4d95dcb21f0fdb4be4dc09b8bf
SongKong00 <hesong1@naver.com>
2023-01-25 17:06:54 +0900

    Fix: 달리기 상태 변경 및 색 변경

--- app/src/main/java/com/example/relay/Constants.kt
+++ app/src/main/java/com/example/relay/Constants.kt
@@ -9,7 +9,8 @@ object Constants {
 
     const val REQUEST_CODE_LOCATION_PERMISSION = 0
 
-    const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
+    const val ACTION_START_SERVICE = "ACTION_START_SERVICE"
+    const val ACTION_RESUME_SERVICE = "ACTION_RESUME_SERVICE"
     const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
     const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
     const val ACTION_SHOW_TRACKING_FRAGMENT = "ACTION_SHOW_TRACKING_FRAGMENT"
@@ -19,8 +20,8 @@ object Constants {
     const val LOCATION_UPDATE_INTERVAL = 10000L
     const val FASTEST_LOCATION_INTERVAL = 5000L
 
-    const val POLYLINE_COLOR = Color.RED
-    const val POLYLINE_WIDTH = 8f
+    const val POLYLINE_COLOR = R.color.orange
+    const val POLYLINE_WIDTH = 10f
     const val MAP_ZOOM = 15f
 
     const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
