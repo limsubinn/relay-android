140d3caaf4a93c2075800e9c9524d940f52d2175
limsubinn <mymyx02@naver.com>
2023-01-25 20:21:03 +0900

    Feat: 그룹 키워드 검색

--- app/src/main/java/com/example/relay/group/GroupListFragment.kt
+++ app/src/main/java/com/example/relay/group/GroupListFragment.kt
@@ -1,15 +1,19 @@
 package com.example.relay.group
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.relay.databinding.FragmentGroupListBinding
 import com.example.relay.group.models.GroupListResponse
 import com.example.relay.group.models.GroupListResult
 import com.softsquared.template.kotlin.src.main.myPage.ListRVAdapter
+import android.view.inputmethod.InputMethodManager
 
 class GroupListFragment: Fragment(), ListInterface {
     private var _binding: FragmentGroupListBinding? = null
@@ -27,8 +31,22 @@ class GroupListFragment: Fragment(), ListInterface {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // 클럽 리스트 받아오기
+        // 클럽 리스트 받아오기 (전체)
         ListService(this).tryGetClubList("")
+
+        // EditText에서 엔터 키를 누르면 검색한 클럽의 리스트를 가져온다.
+        binding.etGroupSearch.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            when (keyCode) {
+                KeyEvent.KEYCODE_ENTER -> {
+                    val search = binding.etGroupSearch.text.toString()
+                    ListService(this).tryGetClubList(search)
+
+                    val inputMethodManager = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+                }
+            }
+            true
+        })
     }
 
     override fun onGetClubListSuccess(response: GroupListResponse) {
@@ -37,11 +55,18 @@ class GroupListFragment: Fragment(), ListInterface {
         // 리사이클러뷰
         val clubList: ArrayList<GroupListResult> = arrayListOf()
         val listAdapter = ListRVAdapter(clubList)
+
         binding.rvGroupAll.adapter = listAdapter
         binding.rvGroupAll.layoutManager = LinearLayoutManager(activity)
+
         if (res != null) {
             clubList.addAll(res)
-            listAdapter.notifyDataSetChanged()
+        }
+
+        listAdapter.notifyDataSetChanged()
+
+        if (listAdapter.itemCount == 0) {
+            Toast.makeText(activity, "검색된 결과가 없습니다.", Toast.LENGTH_SHORT).show()
         }
     }
 
--- app/src/main/java/com/example/relay/group/GroupRetrofit.kt
+++ app/src/main/java/com/example/relay/group/GroupRetrofit.kt
@@ -3,12 +3,12 @@ package com.example.relay.group
 import com.example.relay.group.models.GroupListResponse
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface GroupRetrofit {
     // 그룹 목록 조회 (전체, 검색)
-    @GET("/clubs/{search}")
+    @GET("/clubs")
     fun getClubListRes(
-        @Path("search") search: String
+        @Query("search") search: String
     ) : Call<GroupListResponse>
 }
