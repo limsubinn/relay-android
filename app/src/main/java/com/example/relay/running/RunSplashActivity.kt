c866e6daa76378998828df56c4d40f447371a307
SongKong00 <hesong1@naver.com>
2023-01-27 23:39:36 +0900

    Feat: 달리기 시작 카운트다운 스플래시 화면 생성

---
+++ app/src/main/java/com/example/relay/running/RunSplashActivity.kt
@@ -0,0 +1,42 @@
+package com.example.relay.running
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.relay.R
+import com.example.relay.databinding.ActivityRunSplashBinding
+import com.example.relay.ui.MainActivity
+
+class RunSplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRunSplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+        binding = ActivityRunSplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.tvCount.text = "2"
+        }, 1000)
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.tvCount.text = "3"
+        }, 2000)
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.tvCount.setTextSize(1, 100F)
+            binding.tvCount.text = "출발!"
+        }, 3000)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container_fragment, RunningFragment())
+                .commit()
+        }, 3000)
+    }
+}
