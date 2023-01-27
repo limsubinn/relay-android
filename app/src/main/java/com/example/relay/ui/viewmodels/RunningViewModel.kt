f5b4d5f15f99513c4f3962033ee1a7ff20f758a6
SongKong00 <hesong1@naver.com>
2023-01-27 22:23:51 +0900

    Feat: RoomDB에 저

--- app/src/main/java/com/example/relay/ui/viewmodels/RunningViewModel.kt
+++ app/src/main/java/com/example/relay/ui/viewmodels/RunningViewModel.kt
@@ -2,9 +2,16 @@ package com.example.relay.ui.viewmodels
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.relay.db.Run
 import com.example.relay.repository.RunningRepository
+import kotlinx.coroutines.launch
 
 class RunningViewModel @ViewModelInject constructor(
     val runningRepository: RunningRepository
 ): ViewModel() {
+
+    fun insertRun(run: Run) = viewModelScope.launch {
+        runningRepository.insertRun(run)
+    }
 }
