180a5a740892cd0a3e3948c1512e74446299dc07
limsubinn <mymyx02@naver.com>
2023-01-31 02:18:45 +0900

    Feat: 팀원 페이지 리사이클러뷰 연결
    임시 데이터 추가

--- app/src/main/java/com/example/relay/group/GroupMemberFragment.kt
+++ app/src/main/java/com/example/relay/group/GroupMemberFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.relay.databinding.FragmentGroupMemberBinding
 import com.example.relay.ui.MainActivity
 
@@ -59,6 +60,31 @@ class GroupMemberFragment: Fragment() {
                         "imgURL" to imgURL, "name" to name, "recruitStatus" to recruitStatus))
                 mainActivity?.groupFragmentChange(0) // 그룹 메인으로 이동
             }
+
+            // 리사이클러뷰
+            val memberList: ArrayList<Member> = arrayListOf()
+            val memberAdapter = GroupMemberRVAdapter(memberList)
+
+            binding.rvGroupMember.adapter = memberAdapter
+            binding.rvGroupMember.layoutManager = LinearLayoutManager(activity)
+
+
+            memberList.apply {
+                add(Member("리페", "팀장"))
+                add(Member("노창", "디자이너"))
+                add(Member("비카", "서버"))
+                add(Member("설기", "서버"))
+                add(Member("솜", "서버"))
+                add(Member("채리", "서버"))
+                add(Member("라나", "안드로이드"))
+                add(Member("리미", "안드로이드"))
+                add(Member("샐리", "안드로이드"))
+                add(Member("야옹", "iOS"))
+                add(Member("테오", "iOS"))
+                add(Member("혜콩", "iOS"))
+            }
+
+            memberAdapter.notifyDataSetChanged()
         }
     }
 
---
+++ app/src/main/java/com/example/relay/group/Member.kt
@@ -0,0 +1,6 @@
+package com.example.relay.group
+
+data class Member(
+    val name: String?,
+    val info: String?
+)
