22e73bd76a665f51edca651be475fd83711642e5
limsubinn <mymyx02@naver.com>
2023-02-01 12:12:29 +0900

    Feat: 임시 스피너 설정 (그룹 생성)

--- app/src/main/java/com/example/relay/group/GroupCreateFragment.kt
+++ app/src/main/java/com/example/relay/group/GroupCreateFragment.kt
@@ -1,11 +1,15 @@
 package com.example.relay.group
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import com.example.relay.R
 import com.example.relay.databinding.FragmentGroupCreateBinding
 import com.example.relay.ui.MainActivity
 
@@ -38,6 +42,22 @@ class GroupCreateFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // 스피너 설정 -> 화면 기획 완성되면 수정
+//        val sGoal = resources.getStringArray(R.array.group_goal)
+//        val goalAdapter =
+//            activity?.let { ArrayAdapter<String>(it, android.R.layout.simple_spinner_item, sGoal) }
+//        binding.spGoal.adapter = goalAdapter
+//        binding.spGoal.setSelection(0)
+//        binding.spGoal.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun onNothingSelected(p0: AdapterView<*>?) {
+//                TODO("Not yet implemented")
+//            }
+//        }
 
     }
 
