4ac6f8737d54da9a712f419bc77a92890d18839b
limsubinn <mymyx02@naver.com>
2023-02-11 10:38:55 +0900

    Feat: 멤버 월별 기록

--- app/src/main/java/com/example/relay/group/view/MemberRecordFragment.kt
+++ app/src/main/java/com/example/relay/group/view/MemberRecordFragment.kt
@@ -20,7 +20,7 @@ import com.example.relay.ui.MainActivity
 import java.text.SimpleDateFormat
 
 
-class MemberRecordFragment: Fragment() {
+class MemberRecordFragment: Fragment(), MyRecordInterface {
     private var _binding: FragmentMyRecordBinding? = null
     private val binding get() = _binding!!
 
@@ -32,6 +32,8 @@ class MemberRecordFragment: Fragment() {
     private var status = 0 // 거리, 시간, 속도 선택 상태
     private val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd")
     private var curDate = ""
+    private var year = 0
+    private var month = 0
 
     private var mainActivity: MainActivity? = null
 
@@ -70,36 +72,51 @@ class MemberRecordFragment: Fragment() {
 
             if (curDate.isNotEmpty()) {
                 val selDate = simpleDateFormat.parse(curDate)
-                val year = Integer.parseInt(curDate.substring(0, 4))
-                val month = Integer.parseInt(curDate.substring(5, 7))
+                year = Integer.parseInt(curDate.substring(0, 4))
+                month = Integer.parseInt(curDate.substring(5, 7))
 
                 // Log.d("month record", "$selDate, $year, $month")
 
                 binding.calendarView.addDecorator(activity?.let { SelectDecorator(selDate, it) })
                 binding.calendarView.setCurrentDate(selDate)
+
+                // 월별 기록 불러오기
+                MyRecordService(this).tryGetDailyRecord(year, month, userIdx)
             }
         }
 
         // 탭
         binding.btnDistance.setOnClickListener {
             status = 0
+
             binding.barDistance.visibility = View.VISIBLE
             binding.barTime.visibility = View.INVISIBLE
             binding.barSpeed.visibility = View.INVISIBLE
+
+            // 월별 기록 불러오기
+            MyRecordService(this).tryGetDailyRecord(year, month, userIdx)
         }
 
         binding.btnTime.setOnClickListener {
             status = 1
+
             binding.barDistance.visibility = View.INVISIBLE
             binding.barTime.visibility = View.VISIBLE
             binding.barSpeed.visibility = View.INVISIBLE
+
+            // 월별 기록 불러오기
+            MyRecordService(this).tryGetDailyRecord(year, month, userIdx)
         }
 
         binding.btnSpeed.setOnClickListener {
             status = 2
+
             binding.barDistance.visibility = View.INVISIBLE
             binding.barTime.visibility = View.INVISIBLE
             binding.barSpeed.visibility = View.VISIBLE
+
+            // 월별 기록 불러오기
+            MyRecordService(this).tryGetDailyRecord(year, month, userIdx)
         }
 
         // 날짜 선택
@@ -118,5 +135,309 @@ class MemberRecordFragment: Fragment() {
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    override fun onGetMonthRecordSuccess(response: MonthRecordResponse) {
+        if ((response.isSuccess) && (response.result.isNotEmpty())) {
+            val res = response.result
+
+            var date = ""
+            var value = 0.0
+
+            for (i in res) {
+                when (status) {
+                    0 -> { // 거리
+                        for (i in res) {
+                            date = i.date
+                            value = i.totalDist
+
+                            if ((value > 0.0) && (value < 4.0)) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator1(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator1(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            } else if ((value >= 4.0) && (value < 8.0)) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator2(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator2(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            } else if ((value >= 8.0) && (value < 12.0)) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator3(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator3(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            } else if ((value >= 12.0) && (value < 16.0)) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator4(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator4(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            } else if ((value >= 16.0) && (value < 20.0)) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator5(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator5(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            } else if (value >= 20.0) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator6(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator6(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            }
+                        }
+                    }
+                    1 -> { // 시간
+                        for (i in res) {
+                            date = i.date
+                            value = i.totalTime
+
+                            if ((value > 0.0) && (value < 20.0)) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator1(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator1(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            } else if ((value >= 20.0) && (value < 40.0)) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator2(
+                                            simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator2(
+                                            simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            } else if ((value >= 40.0) && (value < 60.0)) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator3(
+                                            simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator3(
+                                            simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            } else if ((value >= 60.0) && (value < 80.0)) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator4(
+                                            simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator4(
+                                            simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            } else if ((value >= 80.0) && (value < 100.0)) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator5(
+                                            simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator5(
+                                            simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            } else if (value >= 100) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator6(
+                                            simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator6(
+                                            simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            }
+                        }
+                    }
+                    2 -> { // 속도
+                        for (i in res) {
+                            date = i.date
+                            value = i.avgPace
+
+                            if ((value > 0.0) && (value < 2.0)) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator1(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator1(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            } else if ((value >= 2.0) && (value < 4.0)) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator2(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator2(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            } else if ((value >= 4.0) && (value < 6.0)) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator3(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator3(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            } else if ((value >= 6.0) && (value < 8.0)) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator4(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator4(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            } else if ((value >= 8.0) && (value < 10.0)) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator5(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator5(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            } else if (value >= 10.0) {
+                                if (date == curDate) {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        SelectDecorator6(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                } else {
+                                    binding.calendarView.addDecorator(activity?.let {
+                                        Decorator6(simpleDateFormat.parse(date),
+                                            it
+                                        )
+                                    })
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onGetMonthRecordFailure(message: String) {
+        Toast.makeText(activity, "사용자의 기록을 불러오는 데 실패했습니다.", Toast.LENGTH_SHORT).show()
     }
 }
