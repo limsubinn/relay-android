b3b947fe6ba9c6f5d21f07ad5bb7eec1a144b2f4
limsubinn <mymyx02@naver.com>
2023-02-11 22:03:04 +0900

    Fix: 그룹 페이지 정보 조회 API

--- app/src/main/java/com/example/relay/group/models/Member.kt
+++ app/src/main/java/com/example/relay/group/models/Member.kt
@@ -1,9 +1,11 @@
 package com.example.relay.group.models
 
+import com.example.relay.timetable.models.MemSchedule
 import com.google.gson.annotations.SerializedName
 
 data class Member (
     @SerializedName("memberStatusIdx") val statusIdx: Long,
     @SerializedName("runningRecord") val record: ArrayList<RecordWithoutLocation>,
+    @SerializedName("timeTableRes") val timeTableRes: MemSchedule,
     @SerializedName("userProfile") val profile: MemberProfile
 )
